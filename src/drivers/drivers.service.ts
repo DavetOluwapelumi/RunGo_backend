059f1f8cda38fae92c9e8556301370cbba208006
DESKTOP-M5S11J4 <titilayodavid405@gmail.com>
2025-03-15 23:18:58 +0100

    drivers.service.ts done

--- src/drivers/drivers.service.ts
+++ src/drivers/drivers.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Driver } from '../../entities/driver.entity';
+import { Repository } from 'typeorm';
+import { CreateDriverDTO } from '../dto/createDriver';
 
 @Injectable()
-export class DriversService {}
+export class DriverService {
+  @InjectRepository(Driver)
+  private readonly driverRepository: Repository<Driver>;
+
+  public async findOneByEmail(email: string): Promise<Driver> {
+    return await this.driverRepository.findOneBy({ email });
+  }
+
+  public async findOneByIdentifier(identifier: string): Promise<Driver> {
+    return await this.driverRepository.findOneBy({ identifier });
+  }
+
+  public async create(payload: CreateDriverDTO): Promise<Driver> {
+    const { email, password, firstName, lastName } = payload;
+    const newDriver = this.driverRepository.create();
+
+    newDriver.email = email;
+    newDriver.password = password;
+    newDriver.firstName = firstName;
+    newDriver.lastName = lastName;
+
+    return await this.driverRepository.save(newDriver);
+  }
+}
