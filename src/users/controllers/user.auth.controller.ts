525081a2bd95bfbfff4aa2cc718ccd77911267b3
DESKTOP-M5S11J4 <titilayodavid405@gmail.com>
2025-04-04 13:58:21 +0100

    dev: user authentication

---
+++ src/users/controllers/user.auth.controller.ts
@@ -0,0 +1,31 @@
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    Inject,
+    Post,
+    Put,
+    Query,
+    Request,
+} from "@nestjs/common";
+import { UserAuthService } from "../services/user.auth.service";
+import { CreateUserDTO } from "../dto/createUser";
+import { LoginUserDTO } from "../dto/loginUser";
+import { RequestPasswordResetDTO } from "../dto/requestPasswordReset";
+import { SetNewPasswordDTO } from "../dto/setNewPassword";
+import { JwtPayload } from "src/interfaces/jwt";
+
+@Controller ({ version: "1", path: "user/auth" })
+export class UserAuthController {
+    constructor (
+        @Inject(UserAuthService)
+        private readonly userAuthService: UserAuthService,
+    ) {}
+    
+    @HttpCode(201)
+    @Post("register")
+    async createUser(@Body() request: CreateUserDTO) {
+        return this.userAuthService.register(request);
+    }
+}
